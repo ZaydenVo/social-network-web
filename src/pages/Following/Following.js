177f0a3c81599a761f30c15d328f110ce9b6b6d3
VoTranTin <25730154@ms.uit.edu.vn>
2026-05-21 06:53:28 +0700

    chore: init boilerplate with customized CRA, Babel, SASS and Router

---
+++ src/pages/Following/Following.js
@@ -0,0 +1,7 @@
+import styles from './Following.module.scss';
+
+function Following() {
+  return <h2>Following</h2>;
+}
+
+export default Following;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,7 @@
+import styles from './Home.module.scss';
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+export default Home;
