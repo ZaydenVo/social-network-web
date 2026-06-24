39f66e093bdb2f7704f072fce33b3af3080e2b55
ZaydenVo <votrantin@gmail.com>
2026-06-24 08:33:24 +0700

    feat(auth): integrate Sign Up form with register API endpoint

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import styles from './Home.module.scss';
 import { HomeFeed } from './components/HomeFeed';
 import { HomeGuest } from './components/HomeGuest';
 import DefaultLayout from '~/layouts/DefaultLayout/DefaultLayout';
--- src/pages/Home/components/Signup/Signup.js
+++ src/pages/Home/components/Signup/Signup.js
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAnglesLeft } from '@fortawesome/free-solid-svg-icons';
 import { validate } from '~/FormValidator';
 import { useFormCustom } from '~/hooks';
+import httpRequest from '~/utils/httpRequest';
 
 function Signup({ onSwitchMode }) {
   const initialValues = {
@@ -19,7 +20,31 @@ function Signup({ onSwitchMode }) {
     city: '',
   };
 
-  const handleSignup = () => {};
+  const handleSignup = async () => {
+    try {
+      const response = await httpRequest.post('identity/users/registration', {
+        username: values.username,
+        password: values.password,
+        firstName: values.firstName,
+        lastName: values.lastName,
+        dob: values.dob,
+        email: values.email,
+        city: values.city,
+      });
+
+      if (response.status === 200 || response.status === 201) {
+        alert('Register successful! Please log in!');
+        onSwitchMode();
+      }
+    } catch (error) {
+      if (error.response && error.response.data) {
+        const errorMessage = error.response.data.message || 'Please try again!';
+        alert(errorMessage);
+      } else {
+        alert('Please try again later!');
+      }
+    }
+  };
 
   const { values, errors, handleChange, handleSubmit } = useFormCustom(
     initialValues,
